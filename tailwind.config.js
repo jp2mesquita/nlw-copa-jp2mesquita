8b2d2ca4cf3bd5635682145affe4a412fa9b66ab
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-03 07:45:07 -0300

    chore: add collors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily:{
+        sans: 'Roboto, sans-serif'
+      },
+
+      backgroundImage:{
+        app: 'url(/app-bg.png)'
+      },
+      colors:{
+        ignite:{
+          500: '#129e57'
+        },
+        gray:{
+          100: '#e1e1e6',
+          300: '#8d8d99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214'
+        },
+        yellow:{
+          500: '#f7dd43',
+          700: '#e5cd3d'
+        },
+      },
+      
+    },
   },
   plugins: [],
 }
